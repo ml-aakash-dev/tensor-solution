122f655256dc2fb69ba917b836f54abbe98098d7
ml-aakash-dev <ml.aakash.dev@gmail.com>
2021-11-25 06:45:33 +0500

    origin

---
+++ pages/projects/index.js
@@ -0,0 +1,24 @@
+import Layout from '../../components/layout/Layout'
+import Container from '../../components/Projects/Container'
+import ProjectFilters from '../../components/Projects/ProjectFilters'
+import styles from '../../components/Projects/styles.module.css'
+
+
+const Projects = () => {
+    
+
+
+   
+  
+
+  
+    return <Layout>
+        <div className={styles.Projects}>
+            <Container/>
+        </div>
+        <ProjectFilters/>
+        <div className="firstLayer"></div>
+    </Layout>
+}
+
+export default Projects
